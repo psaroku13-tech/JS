4f7a59a48fd83d63ef4fc853fcab87f86e3c7f71
Saro <psaro.ku13@gmail.com>
2026-08-13 18:11:09 +0530

    control

---
+++ control/cf.js
@@ -0,0 +1,49 @@
+//truthy and falsey value
+
+//falsey value
+
+//false,0,-0,,BingInt 999n,"",null,undefined,NaN
+
+//truthy value
+//"0",'false'," space",[],{},function(){}
+
+//check if array is empty or no
+
+const user=[]
+if(user.length===0)
+{
+    console.log("Array is empty");
+}
+
+const emptyObj={};
+
+if(Object.keys(emptyObj).length===0)
+{
+    //Object is empty Object
+console.log("Object is empty");
+}
+
+
+//false==0 --true
+//false == '' --true
+//0=='' --true
+
+
+//nullish colestion operator(??) : null , undefinded
+
+let val1;
+//val1= 5 ?? 10              op-5
+//val1= null ?? 10           op-null
+//val1= undefinded ?? 15     op-undefinded
+
+//val1= null ?? 10 ?? 10     op-10
+
+//val1= null ?? 10 ?? 20     op-10
+
+
+//Tornary operator
+
+//condition ? true : false
+
+
+
